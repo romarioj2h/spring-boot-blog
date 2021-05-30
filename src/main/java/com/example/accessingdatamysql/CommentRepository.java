47a57d09d1654c9195cd2630c7b14ed3b512144a
Romario Huebra <romarioj2h@gmail.com>
2021-05-30 11:13:06 -0300

    Creating some folders

--- src/main/java/com/example/accessingdatamysql/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.accessingdatamysql;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CommentRepository extends CrudRepository<Comment, Integer> {
-    
-}
